6d50d874481010a12578730da465a0fa0a41ccaa
rishabhc32 <rishabhc32@gmail.com>
2018-02-26 06:17:32 +0530

    added C repo

---
+++ C-stuff/function-pointer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+long int add(int x, int y ) {
+	return x+y;
+}
+
+int main() {
+	long int (*add_pointer)(int, int) = NULL;
+
+	add_pointer = &add;
+	printf("%ld\n", add_pointer(2,3));
+
+	add_pointer = add;
+	printf("%ld\n", add_pointer(4,5));
+
+	return 0;
+}
