ab172e96ff6a5908a99ed6459219977cdb8eb221
Rishabh Chauahn <rishabhc32@gmail.com>
2018-06-15 22:37:04 +0300

    simplified

--- pyspark/simpleapp.py
+++ pyspark/simpleapp.py
@@ -1,9 +1,13 @@
 from pyspark import SparkContext
 
+def d(s):
+    print(type(s))
+    return 'h' in s
+
 textFileName = 'yo.txt'
 sc = SparkContext('local', 'Simple App')
 textFile = sc.textFile(textFileName)
 
-number_of_Hs = textFile.filter(lambda s: 'h' in s).count()
+number_of_Hs = textFile.filter(d).count()
 
 print("Lines with h: %i" % (number_of_Hs))
