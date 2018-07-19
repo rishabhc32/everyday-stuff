c3e4cfdbcd35d666d7e22ebd4cd392d116a254ef
Rishabh Chauahn <rishabhc32@gmail.com>
2018-07-19 14:09:22 +0530

    added avl deletion

--- cpp/competitive/binary-search-tree/AVLtree.cpp
+++ cpp/competitive/binary-search-tree/AVLtree.cpp
@@ -75,7 +75,7 @@ node* balanceHeight(node* root, int key) {
         if(key > root->right->data)
             return left_rotate(root);
 
-        else if( key < root->right->data) {
+        else if(key < root->right->data) {
             root->right = right_rotate(root->right);
             return left_rotate(root);
         }
@@ -94,8 +94,64 @@ node* insert(node* root, int key) {
         root->right = insert(root->right, key);
     
     root->height = 1 +  max(get_height(root->left), get_height(root->right));
-    
+
     return balanceHeight(root, key);
+}
+
+node* findInorderSuccessor(node* root) {
+    while(root->left != nullptr)
+        root = root->left;
+    
+    return root;
+}
+
+node* balanceDeleteHeight(node* root, int key) {
+    int bf = get_balanceFactor(root);
+
+    if(bf > 1) {
+        if(get_balanceFactor(root->left) < 0) 
+            root->left = left_rotate(root->left);
+        
+        return right_rotate(root);
+    }
+    else if(bf < -1) {
+        if(get_balanceFactor(root->left) > 0) 
+            root->right = right_rotate(root->right);
+        
+        return left_rotate(root);
+    }
+
+    return root;
+}
+
+node* delete_node(node* root, int key) {
+    if(root == nullptr)
+        return root;
+    
+    if(key < root->data) 
+        root->left = delete_node(root->left, key);
+    else if(key > root->data)
+        root->right = delete_node(root->right, key);
+
+    else {
+        if(root->left == nullptr || root->right == nullptr) {
+            node* temp = root->left == nullptr ?
+                root->right :
+                root->left;
+            
+            delete root;
+            return temp;
+        }
+        else {
+            node* temp = findInorderSuccessor(root->right);
+            root->data = temp->data;
+            root->right = delete_node(root->right, temp->data);
+        }
+    }
+
+    root->height = 1 + max(get_height(root->left), get_height(root->right));
+    
+    return balanceDeleteHeight(root, key);
 }
 
 void preOrder(node *root) {
@@ -109,14 +165,24 @@ void preOrder(node *root) {
 int main() {
     node *root = nullptr;
     
+    root = insert(root, 9);
+    root = insert(root, 5);
     root = insert(root, 10);
-    root = insert(root, 20);
-    root = insert(root, 30);
-    root = insert(root, 40);
-    root = insert(root, 50);
-    root = insert(root, 25);
+    root = insert(root, 0);
+    root = insert(root, 6);
+    root = insert(root, 11);
+    root = insert(root, -1);
+    root = insert(root, 1);
+    root = insert(root, 2);
 
     cout<<"Preorder traversal of the constructed AVL tree is\n";
+    preOrder(root);
+    cout<<endl;
+
+    root = delete_node(root, 0);
+    root = delete_node(root, -1);
+    
+    cout<<"Preorder traversal of the AVL tree after deletion is\n";
     preOrder(root);
     cout<<endl;
     
