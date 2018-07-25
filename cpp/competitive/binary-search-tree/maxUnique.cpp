1070b5f6b9185dbaed75e11206c6014c5bd17095
Rishabh Chauahn <rishabhc32@gmail.com>
2018-07-25 15:40:46 +0530

    Maximum Unique Element in every subarray of size K

---
+++ cpp/competitive/binary-search-tree/maxUnique.cpp
@@ -0,0 +1,48 @@
+// Poblem Statement -- https://www.geeksforgeeks.org/maximum-unique-element-every-subarray-size-k/
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+
+using namespace std;
+
+int main() {
+    vector<int> v{2, 4, 4, 2, 1};
+    map<int, int> m;
+    set<int> s;
+
+    int k = 3, del_pos = 0, n = v.size();
+
+    for(int i = 0 ; i < k-1 ; ++i) {
+        m[v.at(i)]++;
+        if(m[v.at(i)] == 1)
+            s.insert(v.at(i));
+    }
+
+    for(int i = k-1 ; i < n ; ++i) {
+        m[v.at(i)]++;
+
+        if(m[v.at(i)] == 1)
+            s.insert(v.at(i));
+        else 
+            s.erase(v.at(i));
+        
+        if(s.size() == 0)
+            cout<<"Nothing\n ";
+        else    
+            cout<<*s.rbegin()<<" ";
+        
+        m[v.at(del_pos)]--;
+
+        if(m[v.at(del_pos)] == 1)
+            s.insert(v.at(del_pos));
+        if(m[v.at(del_pos)] == 0)
+            s.erase(v.at(del_pos));
+        
+        ++del_pos;
+    }
+
+    cout<<endl;
+    return 0;
+}
