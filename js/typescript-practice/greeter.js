6e7aa6bfd7db3bd8426065e158e76bc793235d21
spooderman <rishabhc32>
2018-08-08 09:40:14 +0530

    added greeter example

---
+++ js/typescript-practice/greeter.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "hello, " + person;
+}
+var user = "RC";
+document.body.innerHTML = greeter(user);
---
+++ js/typescript-practice/greeter.ts
@@ -0,0 +1,7 @@
+function greeter(person: string) {
+    return "hello, " + person
+}
+
+let user = "RC"
+
+document.body.innerHTML = greeter(user)
