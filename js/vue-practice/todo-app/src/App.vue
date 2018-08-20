badba25e18382caf193f0b320098fd94f05b9d5c
rishabhc32 <rishabhc32@gmail.com>
2018-08-20 21:37:04 +0530

    removed hello world and added todo component

--- js/vue-practice/todo-app/src/App.vue
+++ js/vue-practice/todo-app/src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+    <div id="app">
+      <todo-list v-bind:todos="todos"></todo-list>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import TodoList from './components/TodoList'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        TodoList
+    },
+
+    data () {
+        return {
+            todos: [{
+                title: 'Todo A',
+                project: 'Project A',
+                done: false
+            }, {
+                title: 'Todo B',
+                project: 'Project B',
+                done: true
+            }, {
+                title: 'Todo C',
+                project: 'Project C',
+                done: false
+            }]
+        }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#yo {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
