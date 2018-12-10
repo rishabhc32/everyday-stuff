4e9152e743e19009f5d8ae8811dd50732a207854
rishabhc32 <rishabhc32@gmail.com>
2018-12-10 18:15:31 +0530

    vue webpack hello world

---
+++ js/vue-practice/hello-world/src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+    <button v-on:dblclick="increaseCount">You clicked me {{ counter }} times.</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    data: () => {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        increaseCount: function() {this.counter++}  
+    }
+}
+</script>
---
+++ js/vue-practice/hello-world/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h3>Using Vue CLI</h3>
+        <h3>{{ msg }}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  color: green;
+}
+</style>
