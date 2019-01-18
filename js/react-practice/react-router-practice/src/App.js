942cf58e7df6a873c5829aac0c438db62cc730c6
rishabhc32 <rishabhc32@gmail.com>
2019-01-18 19:00:54 +0530

    Secret Route

--- js/react-practice/react-router-practice/src/App.js
+++ js/react-practice/react-router-practice/src/App.js
@@ -1,65 +1,58 @@
 import React, { Component } from 'react';
-import {Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
+import Redirect from 'react-router-dom/Redirect';
 
-const Courses = ({match}) => (
-    <div>
-        <ul>
-            <li><Link to={`${match.url}/technology`}>Technology</Link></li>
-            <li><Link to={`${match.url}/business`}>Business</Link></li>
-            <li><Link to={`${match.url}/economics`}>Economics</Link></li>
-        </ul>
-
-        <Route 
-            exact 
-            path={`${match.path}/:course`} 
-            render={({match}) => (
-                <div> This is {match.params.course} </div>
-            )} 
-        />
-    </div>
+const Public = () => (
+    <div> This is a public page </div>
 );
 
-const Home = () => (
-    <div>
-        <h2>Home</h2>
-    </div>
+const Private = () => (
+    <div> This is private page </div>
 );
 
-const Airport = () => (
-    <div>
-        <ul>
-            <li>Indira Gandhi International Airpot</li>
-            <li>Chhatrapati Shivaji International Airport</li>
-            <li>Kempegowda International Airport</li>
-        </ul>
-    </div>
+const Login = () => (
+    <div> Login Page <button>login</button> </div>
 );
 
-const City = () => (
-    <div>
-        <ul>
-            <li>Delhi</li>
-            <li>Mumbai</li>
-            <li>Bengaluru</li>
-        </ul>
-    </div>
+const AuthService = {
+    isAuthenticated: false,
+    authenticate(cb) {
+        this.authenticate = true;
+        setTimeout(cb, 100);
+    },
+    logout(cb) {
+        this.isAuthenticated = false;
+        setTimeout(cb, 100);
+    }
+}
+
+const SecretRoute = ({component: Component}, ...rest) => (
+    <Route {...rest} render={(props) => (
+        AuthService.isAuthenticated === true
+            ? <Component {...props} />
+            : <Redirect to="/login" />
+    )}
+    
+    />
 );
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/courses">Courses</Link></li>
-                    <li><Link to="/cities">Cities</Link></li>
-                </ul>
+            <Router>
+                <div style={{width: 1000, margin: "0 auto"}}>
+                    <ul>
+                        <li><Link to="/public">Public</Link></li>
+                        <li><Link to="/private">Private</Link></li>
+                    </ul>
 
-                <Route path="/"  exact component={Home} />
-                <Route path="/courses" component={Courses} />
-                <Route path="/cities" component={City} />
-            </div>
+                    <hr />
+
+                    <Route path="/public" component={Public} />
+                    <SecretRoute path="/private" component={Private} />
+                </div>
+            </Router>
         );
     }
 }
